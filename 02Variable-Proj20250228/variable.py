6f550807dd779a4f201a9b8e25d55670e813c554
nitinkumpal <nitinkumpal@gmail.com>
2025-02-28 22:14:02 +0530

    commit for variable

---
+++ 02Variable-Proj20250228/variable.py
@@ -0,0 +1,11 @@
+a=10
+b=10
+
+# prints the value of variable a and b
+print(a,b)  #10 10
+
+# prints the location of the value 10 in the memory
+print(id(a),id(b)) # since value of variable a and b is same i.e. 10 so '10' has the location for both a and b "140721980507336 140721980507336"
+
+#prints a as the string and b is refering to the value 10 so prints 10
+print("a",b)
