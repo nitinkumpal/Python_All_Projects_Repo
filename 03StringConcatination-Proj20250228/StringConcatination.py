e9d8b937d4e4fcb4159bc562cb5411a798003914
nitinkumpal <nitinkumpal@gmail.com>
2025-03-09 09:44:13 +0530

    commit for variable

---
+++ 03StringConcatination-Proj20250228/StringConcatination.py
@@ -0,0 +1,16 @@
+a="Hello"
+b="world"
+# concatinate the string value of both the variable "HelloWorld"
+print(a+b)
+
+# to give space between the 2 string or word
+print(a+" "+b)
+
+# adds the variable and the value and gives the result as 50
+c=30
+print(c+20)
+
+# gives error if you concatinate 2 different datatypes as below- string and integer
+print(a+c) #TypeError: can only concatenate str (not "int") to str
+
+#
